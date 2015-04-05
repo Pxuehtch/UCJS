ee8846a1c2d2bfa101c1db3d386cb6d7144e743e
Pxuehtch <pxuehtch@example.com>
2015-04-05 17:40:13 +0900

    fx37: fix up key names for IME
    * https://bugzilla.mozilla.org/show_bug.cgi?id=900372

--- UCJS_files/217-IMEState.uc.js
+++ UCJS_files/217-IMEState.uc.js
@@ -74,15 +74,9 @@ function handleEvent(aEvent) {
       //
       // TODO: Detect the <Caps Lock> key for opening IME. But the key can be
       // found for closing IME in my case ('Hiragana' is detected).
-      //
-      // TODO: In Fx33, some key values are considered deprecated and warned
-      // in the web console. They will be changed in the future to comply with
-      // the latest DOM3 spec.
-      // @see https://bugzilla.mozilla.org/show_bug.cgi?id=1024864
-      // @see https://bugzilla.mozilla.org/show_bug.cgi?id=900372
       let IMEkeys = [
-        'HalfWidth', // will be 'Hankaku'
-        'FullWidth', // will be 'Zenkaku'
+        'Hankaku',
+        'Zenkaku',
         'Alphanumeric',
         'Convert',
         'Hiragana'
